d7b12bbce30621f5be316be192d02488684a154b
Dmitry <killer_code@bk.ru>
2019-08-11 09:14:37 +0200

    404

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
                 <section>
                     <div class="footer__logo footer__logo-col">
                         <router-link :to="{name: 'iMain'}">
-                            <img src="images/logo.svg" alt="" height="100px">
+                            <img src="/images/logo.svg" alt="" height="40px">
                         </router-link>
                         <small>DinoStore</small>
                     </div>
@@ -35,10 +35,16 @@ a {
 }
 
 .footer {
-    background: grey;
+    background-color: #b0b0b0;
+    background-image: linear-gradient(to top, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0) 100%);
+
+    margin-top: 40px;
+    padding: 10px;
 }
 
-.footer__logo-col {
-    display: block;
+.footer__logo {
+    display: flex;
+    align-items: center;
+    color: black;
 }
 </style>
